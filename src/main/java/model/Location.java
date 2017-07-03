dc05bb545af47e0965ac99248782d36822463a78
handriss <andrashinkel@gmail.com>
2017-07-03 17:34:46 +0200

    Location class first-glance style implemented

--- src/main/java/model/Location.java
+++ src/main/java/model/Location.java
@@ -1,4 +1,43 @@
 package model;
 
+import exception.InvalidLocationNameException;
+
+import java.util.Set;
+
 public class Location {
+
+    private Character name;
+
+    //A child az, akinek a jelenlegi node elott kell jonnie
+    // I am not sure yet if I need this field
+    private Set<Location> children = null;
+
+    private Location parent = null;
+
+    public Location(Character name) throws InvalidLocationNameException {
+        if (!(name >= 'a' && name <= 'z')) {
+            throw new InvalidLocationNameException("Name must be represented by a lowercase character of the English alphabet!");
+        }
+    }
+
+    public Location(String name) throws InvalidLocationNameException{
+
+        if(name.length() > 0){
+            throw new InvalidLocationNameException("Name must be one character long!");
+        }
+        else if (!name.matches("[a-z]")) {
+            throw new InvalidLocationNameException("Name must be represented by a lowercase character of the English alphabet!");
+        }
+        else{
+            this.name = name.charAt(0);
+        }
+    }
+
+    public void setParent(Location parent){
+        this.parent = parent;
+    }
+
+    public Location getParent(){
+        return this.parent;
+    }
 }
